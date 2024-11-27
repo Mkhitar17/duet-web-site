b79528311fb92b5fe4d33a962aeca91ec271c17d
Mkhitar17 <mkhitarpoghosyan17@gmail.com>
2024-11-28 02:35:58 +0400

    footer full design

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,14 +1,27 @@
 import Link from "next/link";
 import styles from "./layout.module.css";
-import Image  from "next/image";
+import Image from "next/image";
 import footerImage from "@/public/duet.svg"
+import footerTel from "@/public/contact.svg"
+import footerMail from "@/public/mail.svg"
+import footerLocation from "@/public/location.svg"
+import footerFB from "@/public/facebook.svg"
+import footerInst from "@/public/instagram.svg"
+import footerWhatsup from "@/public/whatsup.svg"
+import footerTg from "@/public/tg.svg"
+
+
+
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerSize}>
         <div className={styles.footerIcon}>
-          <Image src={footerImage} width={0} height={0} className={styles.footerImages}></Image>
+          <Image src={footerImage}
+            width={0}
+            height={0}
+            className={styles.footerImages} />
         </div>
 
         <div className={styles.footerLinks}>
@@ -26,10 +39,68 @@ export default function Footer() {
 
         <div className={styles.footerContactUs}>
           <h1 className={styles.footerContactUsTitle}>
-          Կապ մեզ հետ
+            Կապ մեզ հետ
           </h1>
 
-          <div className={styles.footerContactUsIcons_text}></div>
+
+          <div className={styles.footerContactUsIcons_text}>
+            <div className={styles.footerContactUsIcons_text_size}>
+
+              <Image
+                src={footerTel}
+                width={0}
+                height={0}
+                className={styles.footerImageIcons}
+              />
+
+              <p className={styles.footerTextStyle}>098 604406</p>
+            </div>
+
+            <div className={styles.footerContactUsIcons_text_size}>
+
+              <Image
+                src={footerMail}
+                width={0}
+                height={0}
+                className={styles.footerImageIcons}
+              />
+              <p className={styles.footerTextStyle}>duet.coffee@gmail.com</p>
+            </div>
+            <div className={styles.footerContactUsIcons_text_size}>
+              <Image
+                src={footerLocation}
+                width={0}
+                height={0}
+                className={styles.footerImageIcons}
+              />
+              <p className={styles.footerTextStyle}>Հաղթանակ թաղամաս, 1 <br/> <span className={styles.footerSpan}> փողոց 48 շինություն</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.footerFollowCont}>
+          <h1 className={styles.footerFollowMe}>Հետևեք մեզ</h1>
+          <div className={styles.footerFollowIcons}>
+          <a href="#">
+            <Image
+            src={footerFB}
+            />
+          </a>
+          <a href="#">
+            <Image
+            src={footerInst}
+            />
+          </a>
+          <a href="#">
+            <Image
+            src={footerWhatsup}/>
+          </a>
+          <a href="#">
+            <Image
+            src={footerTg}
+            />
+          </a>
+          </div>
         </div>
 
 
--- components/sections/production/index.js
+++ components/sections/production/index.js
@@ -3,25 +3,27 @@ import styles from "./index.module.css";
 import Image from "next/image";
 import ProducItem from "@/components/producItem";
 
-import dexc from "@/public/products/dexc.png";
-import kapuyt from "@/public/products/kapuyt.png";
-import kat from "@/public/products/kat.png";
-import limon from "@/public/products/limon-shish.png";
-import sev from "@/public/products/sev.png";
+// import dexc from "@/public/products/dexc.png";
+// import kapuyt from "@/public/products/kapuyt.png";
+// import kat from "@/public/products/kat.png";
+// import limon from "@/public/products/limon-shish.png";
+import sevDuet from "@/public/products/sevduet.png";
 
 
 
 export default function ProducTionSection() {
 
-    const imagesArray = [dexc, kapuyt, kat, limon, sev]
+    const imagesArray = [ sevDuet]
 
 
     return (
         <div className={styles.Container}>
+            {/* button slaqner */}
             <div className={styles.ProductsSlider}>
                 {imagesArray.map((image, index) => (
                     <div  key={index}>
-                    <ProducItem image={image} /> 
+                    <ProducItem image={image}  className={styles.sectionItem}/> 
+                    <div className={styles.testKlor}></div>
                     </div>
                 ))}
             </div>
