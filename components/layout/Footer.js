6c4cd49f445240a55726663cc6834e263572b53a
Mkhitar17 <mkhitarpoghosyan17@gmail.com>
2024-11-27 14:15:38 +0400

    footer  design

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,9 +1,40 @@
+import Link from "next/link";
 import styles from "./layout.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© {new Date().getFullYear()} My Next.js App. All rights reserved.</p>
+      <div className={styles.footerSize}>
+        <div className={styles.footerIcon}>
+          <div className={styles.footerImages}></div>
+        </div>
+
+        <div className={styles.footerLinks}>
+          <ul className={styles.ul}>
+            <Link href='/' className={styles.linkTitle}>Օգտակար հղումներ</Link>
+          </ul>
+          <div className={styles.footerLinksSize}>
+            <Link href="/" className={styles.links}>Մեր մասին</Link>
+            <Link href="/about" className={styles.links}>Արտադրանք</Link>
+            <Link href="/contact" className={styles.links}>Հումք</Link>
+            <Link href="/contact" className={styles.links}>Կապ մեզ հետ</Link>
+          </div>
+
+        </div>
+
+        <div className={styles.footerContactUs}>
+          <h1 className={styles.footerContactUsTitle}>
+          Կապ մեզ հետ
+          </h1>
+
+          <div className={styles.footerContactUsIcons_text}></div>
+        </div>
+
+
+      </div>
+
+
+      <p>© {new Date().getFullYear()} By ArmCoding - © 2024 All Rights Reserved.</p>
     </footer>
   );
 }
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -4,6 +4,7 @@ import styles from "./layout.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
+      
       <nav className={styles.nav}>
         <Link href="/" className={styles.link}>Մեր մասին</Link>
         <Link href="/about" className={styles.link}>Արտադրանք</Link>
