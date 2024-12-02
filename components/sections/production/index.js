8a38226be2632b6dd81751840e15f5d59a2b0bd5
Mkhitar17 <mkhitarpoghosyan17@gmail.com>
2024-12-02 15:51:55 +0400

    slider animation border none and background gradient realized

--- components/sections/production/index.js
+++ components/sections/production/index.js
@@ -1,5 +1,4 @@
 import { useState, useRef } from "react";
-import Link from "next/link";
 import styles from "./index.module.css";
 import ProducItem from "@/components/producItem";
 import dexc from "@/public/products/dexc.png";
@@ -7,15 +6,14 @@ import kapuyt from "@/public/products/kapuyt.png";
 import kat from "@/public/products/kat.png";
 import limon from "@/public/products/limon-shish.png";
 import sevDuet from "@/public/products/sevduet.png";
-
-
 import SectionHeadline from "@/components/sectionHeadline";
 
 export default function ProducTionSection() {
     const imagesArray = [sevDuet, dexc, kapuyt, kat, limon, kat, kat, kat, kat, kat, kat];
     const gridRef = useRef(null);
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+    // Состояние для управления анимацией
+    const [hoveredIndex, setHoveredIndex] = useState(null);
 
     return (
         <div className={styles.Container}>
@@ -30,9 +28,22 @@ export default function ProducTionSection() {
             />
             <div className={styles.ProductsSlider} ref={gridRef}>
                 {imagesArray.map((image, index) => (
-                    <div key={index} >
-                        <ProducItem image={image}  />
-                        <div className={styles.testKlor}></div>
+                    <div
+                        key={index}
+                        onMouseEnter={() => setHoveredIndex(index)} 
+                        onMouseLeave={() => setHoveredIndex(null)} 
+                    >
+                        <ProducItem image={image} className={styles.testImg} />
+                        <div
+                            className={`${styles.testKlor} ${hoveredIndex === index ? styles.animateKlor : ""
+                                }`}
+                        ></div>
+                        {hoveredIndex === index && (
+                            <div className={styles.hoverContent}>
+                                <p className={styles.testText}>200մլ</p>
+                                <button className={styles.buttonTest}>Տեսնել ավելին</button>
+                            </div>
+                        )}
                     </div>
                 ))}
             </div>
