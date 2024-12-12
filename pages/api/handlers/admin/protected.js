794437a21c21eb8c82fea953bf646efeced382c3
Aren-Chilingaryan <arenchilingaryan88@gmail.com>
2024-12-12 15:31:25 +0400

    fixed build error

--- pages/api/handlers/admin/protected.js
+++
@@ -1,7 +0,0 @@
-import { verifyAdminToken } from '@/middlewares/auth';
-
-export default async function handler(req, res) {
-  await verifyAdminToken(req, res, async () => {
-    res.status(200).json({ success: true, message: 'You have access to this protected route.' });
-  });
-}
